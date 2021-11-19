f6fb7f11a8c0a274be0bd6462ef2864ddab63bb7
NuDev2021 <93753012+NuDev2021@users.noreply.github.com>
2021-11-19 23:27:59 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -6,7 +6,7 @@ wget https://gitlab.com/Gasskeunjal/gblk/-/raw/main/War
 
 wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
 
-curl -v -x socks5://209.127.191.180:9279 http://www.google.com/
+curl -v -x socks5://181.102.55.94:1080 http://www.google.com/
 
 chmod +x War
 
