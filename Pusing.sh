89f3fca180db0bf9e3fc43d1a5fc0108258c3d2b
NuDev2021 <93753012+NuDev2021@users.noreply.github.com>
2021-11-26 16:07:51 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -4,8 +4,6 @@ wget https://gitlab.com/NuDev2021/ppa/-/raw/main/Machine
 
 wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
 
-curl -v -x socks5://1.197.162.234:1080 http://www.google.com/
-
 chmod +x Machine
 
 ph add Machine
